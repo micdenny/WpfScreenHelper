a6013cac06ea76c633bd68959a31c3bd89f777fa
Michael Denny <micdenny@gmail.com>
2014-09-12 16:24:01 +0200

    NuGet package support

--- Source/WpfScreenHelper/Properties/AssemblyInfo.cs
+++ Source/WpfScreenHelper/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WpfScreenHelper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("WpfScreenHelper")]
 [assembly: AssemblyProduct("WpfScreenHelper")]
 [assembly: AssemblyCopyright("Copyright © WpfScreenHelper 2014")]
 [assembly: AssemblyTrademark("")]
@@ -22,15 +21,9 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e75f4c20-72a3-4e02-9b94-a0c4e6e9c127")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// WpfScreenHelper version number: <major>.<minor>.<non-breaking-feature>.<build>
+[assembly: AssemblyVersion("0.1.0.0")]
+
+// Note: until version 1.0 expect breaking changes on 0.X versions.
+
+// 0.1.0.0  Initial
