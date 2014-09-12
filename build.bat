ff02de16c2b5e2987249a2cdacd5dcdb361ba873
Michael Denny <micdenny@gmail.com>
2014-09-12 17:10:35 +0200

    Changed target framework to 4.0 to be compatible with old software

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 "C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\msbuild.exe" "Source\WpfScreenHelper.sln" /property:Configuration=Release;OutDir=..\..\bin
 
 rmdir /S /Q Package\lib >nul 2>&1
-xcopy bin\*.* Package\lib\net45 /E /I /R /Y
+xcopy bin\*.* Package\lib\net40 /E /I /R /Y
 
 Tools\NuGet\NuGet.exe pack Package\WpfScreenHelper.nuspec -OutputDirectory Package
 
