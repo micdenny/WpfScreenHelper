bc03c8c13c06ae81e677e608c8ddc9db21a82518
Michael Denny <micdenny@gmail.com>
2021-09-21 19:15:35 +0200

    simplify build batch using dotnet tool

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ echo Removing old packages...
 del Package\*.nupkg >nul 2>&1
 
 echo Building...
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" "WpfScreenHelper.sln" /property:Configuration=Release
+dotnet build -c Release
 
 echo.
 echo ------------------------------------------------
